e3d9456d4ab4c4a43cbe76a9bae11b15f5bb0298
swlsolutions <weilongs931@gmail.com>
2025-09-29 11:38:30 -0400

    enhance some cofiguration stuff

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://pacific-taiga-69560-964a51d44167.herokuapp.com';
+export const BASE_URL = process.env.APP_BASE_URL || 'https://pacific-taiga-69560-964a51d44167.herokuapp.com';
 // export const BASE_URL = 'http://localhost:5000';
